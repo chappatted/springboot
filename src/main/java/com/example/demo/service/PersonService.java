789476cc8910c59b531f11e871bf9b53cd4b2cca
Damien Chappatte <damien.chappatte@he-arc.ch>
2024-01-26 14:48:52 +0100

    Added new endpoint to add an address to a person

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,10 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Address;
 import com.example.demo.entity.Person;
+import com.example.demo.repository.AddressRepository;
 import com.example.demo.repository.PersonRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -12,8 +16,22 @@ import org.springframework.stereotype.Service;
 public class PersonService {
 
     private final PersonRepository personRepository;
+    private final AddressRepository addressRepository;
 
     public Person create(Person person) {
         return personRepository.save(person);
+    }
+
+    public Optional<Address> addAddress(Long personId, Address address) {
+        Optional<Person> person = personRepository.findById(personId);
+
+        if(person.isEmpty()) {
+            return Optional.empty();
+        }
+
+        address.setPerson(person.get());
+        addressRepository.save(address);
+
+        return Optional.of(address);
     }
 }
